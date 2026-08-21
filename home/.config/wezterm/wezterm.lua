05c65abad0647c218417506a7487311c4862536d
Heecheon Park <heecheon92@gmail.com>
2026-08-21 17:01:49 +0900

    feat(wezterm): add blinking cursor

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -5,10 +5,15 @@ local config = wezterm.config_builder()
 config.color_scheme = "Monokai Pro (Gogh)"
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 14.0
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+
+config.default_cursor_style = "BlinkingBlock"
+config.cursor_blink_rate = 200
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
 
 -- Dim unfocused windows so the focused one is obvious at a glance.
 local UNFOCUSED_FOREGROUND_TEXT_HSB = { hue = 1.0, saturation = 0.25, brightness = 0.45 }
