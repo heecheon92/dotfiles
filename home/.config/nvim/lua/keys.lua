249aa5b3bbbdab4afd1926d417a6144da8375553
Heecheon Park <heecheon92@gmail.com>
2026-09-15 14:21:49 +0900

    feat(nvim): accept first completion with Ctrl-Y

--- home/.config/nvim/lua/keys.lua
+++ home/.config/nvim/lua/keys.lua
@@ -4,3 +4,11 @@ vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select All' })
 vim.cmd([[ xnoremap <expr> p 'pgv"'.v:register.'y' ]])
 
 vim.keymap.set({ 'n', 'x', 'i' }, '<C-.>', vim.lsp.buf.code_action, { desc = 'Code actions' })
+
+vim.keymap.set('i', '<C-y>', function()
+  if vim.fn.pumvisible() == 1
+      and vim.fn.complete_info({ 'selected' }).selected == -1 then
+    return '<C-n><C-y>'
+  end
+  return '<C-y>'
+end, { expr = true, desc = 'Accept selected or first completion' })
